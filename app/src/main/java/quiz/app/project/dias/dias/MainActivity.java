eb2659dbf5eb867ec8c1dafb0daca6d1d6a75758
Just a Student <121408235+JustaStudent01@users.noreply.github.com>
2023-03-22 14:59:44 +0000

    Update on main screen

--- app/src/main/java/quiz/app/project/dias/dias/MainActivity.java
+++ app/src/main/java/quiz/app/project/dias/dias/MainActivity.java
@@ -2,7 +2,9 @@ package quiz.app.project.dias.dias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logo_advance(View view) {
+        Intent intent = new Intent(this, quiz_login.class);
+        startActivity(intent);
+    }
+
+    public void layout_advance(View view) {
+        Intent intent = new Intent(this, quiz_login.class);
+        startActivity(intent);
+    }
+
+    public void label_advance(View view) {
+        Intent intent = new Intent(this, quiz_login.class);
+        startActivity(intent);
     }
 }
