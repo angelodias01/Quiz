cd98a84645aacf884a7da59ed38d931383009e00
Just a Student <121408235+JustaStudent01@users.noreply.github.com>
2023-03-22 11:37:58 +0000

    Add first files

---
+++ app/src/main/java/quiz/app/project/dias/dias/quiz_login.java
@@ -0,0 +1,14 @@
+package quiz.app.project.dias.dias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quiz_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_login);
+    }
+}
