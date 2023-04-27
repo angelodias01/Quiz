83bf083cccb09d24a87fc64a06712e6a12edec7a
Just a Student <121408235+JustaStudent01@users.noreply.github.com>
2023-04-27 14:37:51 +0100

    update on fragments

--- app/src/main/java/quiz/app/project/dias/dias/MainMenuAdmin/MainMenuAdmin.java
+++ app/src/main/java/quiz/app/project/dias/dias/MainMenuAdmin/MainMenuAdmin.java
@@ -1,4 +1,17 @@
 package quiz.app.project.dias.dias.MainMenuAdmin;
 
-public class MainMenuAdmin {
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import quiz.app.project.dias.dias.R;
+
+public class MainMenuAdmin extends AppCompatActivity {
+    @SuppressLint("SourceLockedOrientationActivity")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main_menu_admin);
+    }
 }
