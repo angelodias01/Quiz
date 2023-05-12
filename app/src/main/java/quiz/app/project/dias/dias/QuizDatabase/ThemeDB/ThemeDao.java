6b282bed403c2a1ab85ca4d3dcd8e54cedc1158f
Just a Student <121408235+JustaStudent01@users.noreply.github.com>
2023-05-12 23:23:06 +0100

    update on roomDB

--- app/src/main/java/quiz/app/project/dias/dias/QuizDatabase/ThemeDB/ThemeDao.java
+++ app/src/main/java/quiz/app/project/dias/dias/QuizDatabase/ThemeDB/ThemeDao.java
@@ -3,16 +3,14 @@ package quiz.app.project.dias.dias.QuizDatabase.ThemeDB;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
-import androidx.room.Query;
 import androidx.room.Update;
 
 @Dao
 public interface ThemeDao {
-
     @Insert
-    void insert(Theme theme);
+    void insertAll(Theme theme);
     @Update
-    void update(Theme theme);
+    void updateAll(Theme theme);
     @Delete
-    void delete(Theme theme);
+    void deleteAll(Theme theme);
 }
