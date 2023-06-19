bdff0f1fb62e890c3e97fce310e20632ecba030b
Just a Student <121408235+JustaStudent01@users.noreply.github.com>
2023-06-19 15:03:39 +0100

    update on roomDB

---
+++ app/src/main/java/quiz/app/project/dias/dias/MainMenuUser/MainPageFragment.java
@@ -0,0 +1,49 @@
+package quiz.app.project.dias.dias.MainMenuUser;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import quiz.app.project.dias.dias.R;
+
+public class MainPageFragment extends Fragment {
+
+    private int userId;
+
+    public MainPageFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainPageFragment newInstance(int userId) {
+        MainPageFragment fragment = new MainPageFragment();
+        Bundle args = new Bundle();
+        args.putInt("userId", userId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            userId = getArguments().getInt("userId");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Use the userId as needed in your fragment
+        // ...
+    }
+}
