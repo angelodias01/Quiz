e6e825d6bce8eb759ef39e13474cbf2d167fb77c
Just a Student <121408235+JustaStudent01@users.noreply.github.com>
2023-07-13 17:35:06 +0100

    updates on fragments

--- app/src/main/java/quiz/app/project/dias/dias/MainMenuUser/ProfileAdapter.java
+++ app/src/main/java/quiz/app/project/dias/dias/MainMenuUser/ProfileAdapter.java
@@ -1,18 +1,22 @@
 package quiz.app.project.dias.dias.MainMenuUser;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
-
 import quiz.app.project.dias.dias.QuizDatabase.AchievementUserDB.AchievementUser;
 import quiz.app.project.dias.dias.QuizDatabase.AchievementsDB.Achievements;
-import quiz.app.project.dias.dias.QuizDatabase.AchievementsDB.AchievementsDao;
 import quiz.app.project.dias.dias.R;
 
 public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder> {
@@ -33,40 +37,72 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileV
 
     @Override
     public void onBindViewHolder(@NonNull ProfileViewHolder holder, int position) {
-        AchievementUser achievementUser = achievementsUserList.get(position);
-        Achievements achievements = getAchievementById(achievementUser.getAchievementId());
+        Achievements achievements = achievementsList.get(position);
+        AchievementUser achievementUser = getAchievementUserById(achievements.getAchievementId());
         holder.bind(achievements);
     }
 
     @Override
     public int getItemCount() {
-        return achievementsUserList.size();
+        return achievementsList.size();
     }
 
     public class ProfileViewHolder extends RecyclerView.ViewHolder {
         TextView lblAchievement;
         TextView lblDescription;
+        TextView lblAchievementDate;
+        ConstraintLayout constraintLayout;
 
         public ProfileViewHolder(@NonNull View itemView) {
             super(itemView);
             lblAchievement = itemView.findViewById(R.id.lblAchievement);
             lblDescription = itemView.findViewById(R.id.lblDescription);
+            lblAchievementDate = itemView.findViewById(R.id.lblAchievementDate);
+            constraintLayout = itemView.findViewById(R.id.ConstraintLayout);
         }
 
         public void bind(Achievements achievements) {
-            if (lblAchievement != null && achievements != null) {
-                String achievementName = achievements.getAchievementName();
-                String achievementDescription = achievements.getDescription();
-                lblAchievement.setText(achievementName);
-                lblDescription.setText(achievementDescription);
+            lblAchievement.setText(achievements.getAchievementName());
+            lblDescription.setText(achievements.getDescription());
+
+            AchievementUser achievementUser = getAchievementUserById(achievements.getAchievementId());
+            if (achievementUser != null) {
+                constraintLayout.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F9B01A")));
+
+                long dateEarned = achievementUser.getDateEarned();
+                Date date = new Date(dateEarned);
+                SimpleDateFormat sdf = new SimpleDateFormat(" | dd/MM/yyyy | HH:mm |");
+                String formattedDate = sdf.format(date);
+                lblAchievementDate.setText(formattedDate);
+                lblAchievementDate.setVisibility(View.VISIBLE);
+            } else {
+                constraintLayout.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#999997")));
+                lblAchievementDate.setVisibility(View.GONE);
             }
         }
-    }
 
+
+        private boolean checkUserHasAchievement(int achievementId) {
+            for (AchievementUser achievementUser : achievementsUserList) {
+                if (achievementUser.getAchievementId() == achievementId) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
     private Achievements getAchievementById(int achievementId) {
         for (Achievements achievement : achievementsList) {
             if (achievement.getAchievementId() == achievementId) {
                 return achievement;
+            }
+        }
+        return null;
+    }
+    private AchievementUser getAchievementUserById(int achievementId) {
+        for (AchievementUser achievementUser : achievementsUserList) {
+            if (achievementUser.getAchievementId() == achievementId) {
+                return achievementUser;
             }
         }
         return null;
